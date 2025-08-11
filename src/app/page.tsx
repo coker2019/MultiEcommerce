5b88714bf250ba4f7f4e4e5fa8f4da3498d767ed
Maurice <your.email@example.com>
2025-08-11 18:26:02 +0100

    Customized major components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
-
-
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
- return (
-<div>
- <p className="text-rose-500">
-  Hello World
- </p>
-</div>
- )
+  return (
+    <div className=" flex flex-col gap-y-4 p-4">
+      <div>
+        <Input placeholder="i be input"></Input>
+      </div>
+
+      <div>
+        <Button variant="elevated">i am a button</Button>
+      </div>
+
+      <div>
+        <Progress value={50}></Progress>
+      </div>
+      <div>
+        <Textarea placeholder="i m a textarea"></Textarea>
+      </div>
+      <div>
+        <Checkbox></Checkbox>
+      </div>
+    </div>
+  );
 }
